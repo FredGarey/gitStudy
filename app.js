595b3dad5dc1bbfb81f30630dff2a6e4f41faa4d
kasian <kirita_2004@icloud.com>
2024-10-20 17:46:44 +0300

    added recursion

---
+++ app.js
@@ -0,0 +1,15 @@
+
+function translit(){
+
+}
+function solution(number){
+    if(number < 0){
+       return 0
+      }
+  }
+
+
+  function requestIdleCallback(n){
+    if(n === 5) return 1
+    return requestIdleCallback()
+  }
